f237cc695c4535b168840a9874b43180c842e795
SpandanaMadimi7 <spandhana178@gmail.com>
2025-12-04 19:40:52 +0530

    Practicing Polymorphism

---
+++ Polymorphism/CalculatorPolymorphism.cs
@@ -0,0 +1,33 @@
+﻿namespace Polymorphism
+{
+    public class CalculatorPolymorphism
+    {
+        public void Calculation()
+        {
+            bool shallAddOnlyPositive = false;
+            var numbers = new List<int> { 2, -1, 6, -10, 30, -5 };
+            var calc = shallAddOnlyPositive ? new PositiveNumbersCalculation() :  new NumbersCalculation();
+           Console.WriteLine(calc.Calculate(numbers));
+        }
+        public class NumbersCalculation
+        {
+            public int Calculate(List<int> numbers)
+            {
+                var sum = 0;
+                foreach (var number in numbers) {
+                    if(IsPositive(number))
+                    sum += number;
+                }
+                return sum;
+            }
+            protected virtual bool IsPositive(int number) {
+                return true;
+            }
+        }
+        public class PositiveNumbersCalculation: NumbersCalculation {
+            protected override bool IsPositive(int number) {
+                return number > 0;
+            }
+        }
+    }
+}
--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -1,30 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-var ingredients = new List<Ingredient>
-{
-    new Cheddar(),
-    new Mozzarella(),
-};
+﻿using Polymorphism;
 
-foreach (var ingredient in ingredients)
-{
-    Console.WriteLine(ingredient.Name);
-}
-public class Ingredient
-{
-    public virtual string Name { get; } = "Generic Ingredient";
-}
+// PizzaPractice pizza = new PizzaPractice();
+// pizza.Pizzas();
 
-
-//Example of inheritance - Cheddar class inherits from Ingredient class and the name 
-// Output - will be "Generic Ingredient"
-public class Cheddar : Ingredient
-{
-  //  public override string Name => "Cheddar Cheese";
-}
-
-// Example of inheritance - Mozzarella class inherits from Ingredient class and overrides the name property
-// Output - will be "Mozzarella Cheese"
-public class Mozzarella : Ingredient
-{
-    public override string Name => "Mozzarella Cheese";
-}
+CalculatorPolymorphism calculatorPolymorphism = new CalculatorPolymorphism();
+calculatorPolymorphism.Calculation();
