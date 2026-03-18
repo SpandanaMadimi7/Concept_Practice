9a805666d89f939a73758efc2a82ec72ff4bdb7d
SpandanaMadimi7 <spandhana178@gmail.com>
2026-03-18 22:39:25 +0530

    Extensions of Enum Practice.

---
+++ Polymorphism/ExtensionsPractice/EnumExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism.ExtensionsPractice
+{
+    public static class EnumExtensions
+    {
+        public static Season Next(this Season season){
+            int value = (int)season;
+            int next = (value + 1) % 4;
+            return (Season)next;
+        }
+    }
+}
--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -1,7 +1,19 @@
 ﻿using Polymorphism;
+using Polymorphism.ExtensionsPractice;
 
 // PizzaPractice pizza = new PizzaPractice();
 // pizza.Pizzas();
 
 CalculatorPolymorphism calculatorPolymorphism = new CalculatorPolymorphism();
 calculatorPolymorphism.Calculation();
+Console.WriteLine("Next season of Winter: " + Season.Winter.Next());
+Console.WriteLine("Next season of Winter: " + Season.Summer.Next());
+Console.WriteLine("Next season of Winter: " + Season.Fall.Next());
+public enum Season
+{
+    Spring,
+    Summer,
+    Fall,
+    Winter
+}
+
